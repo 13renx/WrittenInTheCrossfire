34c5528e28526b61d9b69e700fa2b5d662c9a210
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-06-09 17:57:51 +0800

    Add project files.

---
+++ WrittenInTheCrossfire/curl1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <curl/curl.h>
+
+int main()
+{
+	CURL* curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl == NULL)
+	{
+		std::cout << "HTTP request failed!";
+		return -1;
+	}
+
+	curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/");
+
+	res = curl_easy_perform(curl);
+
+	if (res != CURLE_OK)
+	{
+		std::cout << "Error: " << curl_easy_strerror(res);
+		return -1;
+	}
+
+	curl_easy_cleanup(curl);
+
+	return 0;
+}

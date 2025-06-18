dbd09d92eed0a5dc5f7693f98c15c8c5477f74d7
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-06-18 22:04:31 +0800

    Updated Prototype2 files

--- Prototypes/Prototype2/Prototype2/Prototype2.cpp
+++ Prototypes/Prototype2/Prototype2/Prototype2.cpp
@@ -1,8 +1,30 @@
-#include <SFML/Graphics.hpp>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <string>
+#include <dotenv.h>
+#include <cpr/cpr.h>
+#include <fmt/base.h>
 #include <fmt/format.h>
+#include <nlohmann/json.hpp>
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using json = nlohmann::json;
 
 int main() {
+	dotenv::init(".env");
+	const char* apiKeyTemp = std::getenv("API_KEY");
+
+	if(apiKeyTemp == nullptr) {
+		throw std::runtime_error("API_KEY was not found");
+	}
+
+	const std::string apiKey = std::string(apiKeyTemp);
+	std::string textString = "";
+	json data;
+	json output;
+	std::string outputText;
+
 	sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "Prototype 2");
 
 	sf::Texture backgroundTexture("Textures/background.png");
@@ -13,8 +35,6 @@ int main() {
 	sf::Text text(font);
 	text.setCharacterSize(100);
 	text.setFillColor(sf::Color::Black);
-
-	std::string textString = "";
 
 	while(window.isOpen()) {
 		while(const auto event = window.pollEvent()) {
@@ -38,6 +58,32 @@ int main() {
 				
 				if(!sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Backspace) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) {
 					textString += tempString;
+				}
+
+				if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::RShift) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) {
+					if(textString != "") {
+						data = json::parse(R"(
+							{ 
+								"contents": [
+									{ 
+										"parts": []
+									}
+								] 
+							}
+						)");
+						data["contents"][0]["parts"][0]["text"] = textString;
+
+						cpr::Response res = cpr::Post(cpr::Url{ "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" },
+							cpr::Parameters{ { "key", apiKey } },
+							cpr::Header{ { "Content-Type", "application/json" } },
+							cpr::Body{ data.dump() });
+
+						// Add error handling for response
+
+						output = json::parse(res.text);
+						outputText = output["candidates"][0]["content"]["parts"][0]["text"].template get<std::string>();
+						fmt::print("{}\n", outputText);
+					}
 				}
 
 				text.setString(textString);

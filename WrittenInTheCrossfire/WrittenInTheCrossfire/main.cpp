513d9466033a9259ae7ee5ce88af8f8ec458af9e
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-08-30 16:01:18 +0800

    [WICF] Arrange main headers

--- WrittenInTheCrossfire/WrittenInTheCrossfire/main.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/main.cpp
@@ -1,13 +1,14 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
+#include <iostream>
 #include <string>
 #include <dotenv.h>
 #include <cpr/cpr.h>
 #include <fmt/base.h>
 #include <fmt/format.h>
 #include <nlohmann/json.hpp>
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Client.h"
 
 using json = nlohmann::json;
 

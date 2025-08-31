a7f587255fa0b4ba6ce968f4de6821602a490f8c
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-08-31 17:47:11 +0800

    [WICF] Add apiKey init and change its type

--- WrittenInTheCrossfire/WrittenInTheCrossfire/Client.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/Client.cpp
@@ -9,10 +9,21 @@ using json = nlohmann::json;
 class Client {
 	std::vector<json> history;
 	json prompt;
-	const std::string apiKey;
+	const char* apiKey;
 
 	public:
 		Client() {
+			// Set apiKey with API_KEY from .env file
+			dotenv::init(".env");
+			apiKey = std::getenv("API_KEY");
 
+			try {
+				if(apiKey == nullptr) {
+					throw std::runtime_error("API_KEY was not found");
+				}
+			}
+			catch(const std::runtime_error& e) {
+				std::cout << "Exception: " << e.what() << std::endl;
+			} 
 		}
 };

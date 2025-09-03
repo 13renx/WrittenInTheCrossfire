1283afb36e4dea9c17827cc6080ee3507cd12dcb
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-09-03 09:01:27 +0800

    [WICF] Moved Client declaration

--- WrittenInTheCrossfire/WrittenInTheCrossfire/Client.h
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/Client.h
@@ -1,4 +1,15 @@
 #pragma once
-class Client {
+#include <nlohmann/json.hpp>
+#include <vector>
 
+using json = nlohmann::json;
+
+class Client {
+    std::vector<json> history;
+    json prompt;
+    std::string apiKey;
+
+    public:
+        Client();
+        void setApiKey(const std::string& key);
 };

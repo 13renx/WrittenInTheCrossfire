4b8f69bfb275b296b94b4baf6562dd1fe8b775d6
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-09-03 09:04:05 +0800

    [WICF] Replaced Client declarations

--- WrittenInTheCrossfire/WrittenInTheCrossfire/Client.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/Client.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "Client.h"
 #include <string>
 #include <vector>
@@ -6,24 +8,11 @@
 
 using json = nlohmann::json;
 
-class Client {
-	std::vector<json> history;
-	json prompt;
-	const char* apiKey;
+Client::Client() {
 
-	public:
-		Client() {
-			// Set apiKey with API_KEY from .env file
-			dotenv::init(".env");
-			apiKey = std::getenv("API_KEY");
+}
 
-			try {
-				if(apiKey == nullptr) {
-					throw std::runtime_error("API_KEY was not found");
-				}
-			}
-			catch(const std::runtime_error& e) {
-				std::cout << "Exception: " << e.what() << std::endl;
-			} 
-		}
-};
+void Client::setApiKey(const std::string& key) {
+	apiKey = key;
+
+}

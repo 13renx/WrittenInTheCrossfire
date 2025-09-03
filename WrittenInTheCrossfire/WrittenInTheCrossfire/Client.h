84e9dbfc9764048a4b628bf80bdb9222104eae6a
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-09-03 11:18:12 +0800

    [WICF] Added Client declarations

--- WrittenInTheCrossfire/WrittenInTheCrossfire/Client.h
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/Client.h
@@ -7,9 +7,11 @@ using json = nlohmann::json;
 class Client {
     std::vector<json> history;
     json prompt;
+    json testPrompt;
     std::string apiKey;
 
     public:
         Client();
-        void setApiKey(const std::string& key);
+        void setApiKey(const std::string& apiKey);
+        void fetchResponse(const std::string& promptType);
 };

4d836539cc8d67877918fbc3df56b8d6780c657d
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-10-02 21:45:16 +0800

    [W] Update CampView, TableView, and Client

--- WrittenInTheCrossfire/WrittenInTheCrossfire/CampView.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/CampView.cpp
@@ -10,19 +10,26 @@
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 
-CampView::CampView(Client& client, tgui::Gui& gui, ViewManager* viewManager) : View(gui, viewManager, tgui::Texture::Texture("Assets/Textures/Backgrounds/full_body_bg.PNG")), client(client) {
+CampView::CampView(Client& client, tgui::Gui& gui, ViewManager* viewManager) : View(gui, viewManager, tgui::Texture::Texture("Assets/Textures/Backgrounds/CampView.PNG")), client(client) {
 	gameModel = GameModel();
+	sf::Window* window = gui.getWindow();
 
-    buttonLayoutOne = tgui::HorizontalLayout::create({ 200, 300 });
+    buttonLayoutOne = tgui::HorizontalLayout::create({ 500, 100 });
     writeButton = tgui::Button::create("WRITE A LETTER");
 	dontWriteButton = tgui::Button::create("DON'T WRITE A LETTER");
-	buttonLayoutTwo = tgui::VerticalLayout::create({ 300, 200 });
+	buttonLayoutTwo = tgui::VerticalLayout::create({ 240, 220 });
     cancelButton = tgui::Button::create("CANCEL");
 	selectButton = tgui::Button::create("SELECT");
 	
     buttonLayoutOne->getRenderer()->setSpaceBetweenWidgets(20);
+	buttonLayoutOne->setPosition((tgui::bindWidth(gui) - tgui::bindWidth(buttonLayoutOne) + 20) / 2.0f, (tgui::bindHeight(gui) - tgui::bindHeight(buttonLayoutOne)) / 2.0f);
     buttonLayoutTwo->getRenderer()->setSpaceBetweenWidgets(20);
     buttonLayoutTwo->setVisible(false);
+
+	writeButton->onClick([=, &gui] {
+		window->setMouseCursor(sf::Cursor(sf::Cursor::Type::Arrow));
+		this->viewManager->changeView(ViewManager::ViewType::TABLE_VIEW);
+	});
 
 	mainPanel->add(buttonLayoutOne);
 	mainPanel->add(buttonLayoutTwo);
--- WrittenInTheCrossfire/WrittenInTheCrossfire/TableView.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/TableView.cpp
@@ -10,20 +10,34 @@
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 
-TableView::TableView(Client& client, tgui::Gui& gui, ViewManager* viewManager) : View(gui, viewManager, tgui::Color::Black), client(client) {
+TableView::TableView(Client& client, tgui::Gui& gui, ViewManager* viewManager) : View(gui, viewManager, tgui::Texture::Texture("Assets/Textures/Backgrounds/TableView.PNG")), client(client) {
 	gameModel = GameModel();
+	sf::Window* window = gui.getWindow();
 
 	dearLabel = Widgets::Labels::createLabel("Dear Mom,", 30, 0, 0);
 	letterTextArea = tgui::TextArea::create();
-	buttonLayout = tgui::VerticalLayout::create({ 300, 200 });
+	buttonLayout = tgui::VerticalLayout::create({ 240, 220 });
 	cancelButton = tgui::Button::create("CANCEL WRITING");
 	sendButton = tgui::Button::create("SEND LETTER");
 
-	letterTextArea->setSize(800, 300);
+	dearLabel->setPosition((tgui::bindWidth(gui) - tgui::bindWidth(letterTextArea)) / 2.0f, tgui::bindHeight(gui) - tgui::bindHeight(letterTextArea) - 40);
+	letterTextArea->setSize(730, 647);
 	letterTextArea->setTextSize(30);
+	letterTextArea->setPosition((tgui::bindWidth(gui) - tgui::bindWidth(letterTextArea)) / 2.0f, tgui::bindHeight(gui) - tgui::bindHeight(letterTextArea));
+	letterTextArea->setMaximumCharacters(1500);
 	//letterTextArea->setVisible(false);
 	buttonLayout->getRenderer()->setSpaceBetweenWidgets(20);
+	buttonLayout->setPosition(tgui::bindWidth(gui) - tgui::bindWidth(buttonLayout), tgui::bindHeight(gui) - tgui::bindHeight(buttonLayout));
 
+	cancelButton->onClick([=, &gui] {
+		window->setMouseCursor(sf::Cursor(sf::Cursor::Type::Arrow));
+		this->viewManager->changeView(ViewManager::ViewType::CAMP_VIEW);
+	});
+	sendButton->onClick([] {
+
+	});
+
+	mainPanel->add(dearLabel);
 	mainPanel->add(letterTextArea);
 	mainPanel->add(buttonLayout);
 	buttonLayout->add(cancelButton);

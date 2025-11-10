ee9ddb8388ed57d77a2358fa2e43a411e5a0842c
brent-soan <147591053+brent-soan@users.noreply.github.com>
2025-11-10 10:37:27 +0800

    Add debug mode

--- WrittenInTheCrossfire/WrittenInTheCrossfire/main.cpp
+++ WrittenInTheCrossfire/WrittenInTheCrossfire/main.cpp
@@ -2,6 +2,12 @@
 #include <TGUI/TGUI.hpp>
 #include <TGUI/Backend/SFML-Graphics.hpp>
 
+#ifdef _DEBUG
+	bool debugMode = true;
+#else
+	bool debugMode = false;
+#endif
+
 int main() {
 	tgui::Theme::setDefault("Theme.txt");
 
